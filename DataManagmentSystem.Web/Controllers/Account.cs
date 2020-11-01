9bbf890b2f30bb5cbee76107235a90582ce8f2fe
DESKTOP-OMVH14T\GuruRafiki <broaden.your@outlook.com>
2020-11-01 18:27:35 +0200

    #inital commit

---
+++ DataManagmentSystem.Web/Controllers/Account.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Extensions.CognitoAuthentication;
+using DataManagmentSystem.Web.Models.Account;
+using Amazon.AspNetCore.Identity.Cognito;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataManagmentSystem.Web.Controllers
+{
+    public class Account : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _cognitoUserPool;
+        public Account(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool cognitoUserPool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _cognitoUserPool = cognitoUserPool;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SignUp()
+        {
+            var model = new SignUpModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _cognitoUserPool.GetUser(model.Email);
+                if(user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists.");
+                    return View(model);
+                }
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Confirm");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError(item.Code, item.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Confirm()
+        {
+            var model = new ConfirmModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if(user == null)
+                {
+                    ModelState.AddModelError("NotFound", "A user with the given email address was not found.");
+                    return View(model);
+                }
+
+                var result = await (_userManager as CognitoUserManager<CognitoUser>).ConfirmSignUpAsync(user, model.Code, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError(item.Code, item.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ DataManagmentSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataManagmentSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
